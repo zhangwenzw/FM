f0400d0b5121f03c939ffc97f284b6a761949680
zhangwenzw <604639246@qq.com>
2016-12-16 17:30:10 +0800

    add

---
+++ js/fm.js
@@ -0,0 +1,14 @@
+$(function(){
+	resize(1334,"y");
+	var myScroll = new IScroll('#wrapper', { mouseWheel: true });
+		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+	var mySwiper = new Swiper('.banner', {
+	    pagination: '.swiper-pagination',
+	    paginationClickable :true,
+	})	
+	$("footer>a").click(function(){
+		var index =$("footer>div").index(this);
+		$("footer>a").removeClass("active").eq(index).addClass("active");
+		
+	})
+})
